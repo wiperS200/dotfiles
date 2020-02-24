83d10d5988ece51c32420c2c58cbc4a5bfd6117e
wiper <waipuwiper@gmail.com>
2020-02-25 01:10:04 +0900

    Add my X240's unique settings

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,7 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.irbrc ~/.irbrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.xbindkeysrc ~/.xbindkeysrc
+ln -sf ~/dotfiles/.xprofile ~/.xprofile
+ln -sf ~/dotfiles/.Xmodmap ~/.Xmodmap
