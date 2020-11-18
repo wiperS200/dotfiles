df2b4a79e6dbdedabaa6352d3e885d48e377eff7
waipu <waipuwiper@gmail.com>
2020-11-18 13:34:01 +0900

    Change X settings

--- .bash_profile
+++ .bash_profile
@@ -3,3 +3,4 @@ eval "$(rbenv init -)"
 #export DISPLAY=localhost:0.0
 test -r ~/.bashrc && . ~/.bashrc
 export GOPATH=~/go
+[[ -z $DISPLAY && XDG_VTNR -eq 1 ]] && exec startx
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,5 +4,7 @@ ln -sf ~/dotfiles/.irbrc ~/.irbrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.xbindkeysrc ~/.xbindkeysrc
 ln -sf ~/dotfiles/.xprofile ~/.xprofile
+ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.Xmodmap ~/.Xmodmap
 ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
