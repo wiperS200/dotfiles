391b85f35a44c39645dcffdb77f27039d8311735
waipu <waipu@wiper.dev>
2023-01-03 23:37:14 +0900

    Add git branch print on fish

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt --description 'Write out the prompt'
-#  printf '%s %s\n> ' (prompt_login) (prompt_pwd)
-  printf '%s@%s %s\n> ' (set_color brgreen)(echo $USER) (set_color bryellow)(prompt_hostname) (set_color cyan)(prompt_pwd)
+  printf '%s %s%s\n> ' (prompt_login) (set_color cyan)(prompt_pwd) (set_color white)(fish_git_prompt)
+#  printf '%s@%s %s\n> ' (set_color brgreen)(echo $USER) (set_color bryellow)(prompt_hostname) (set_color cyan)(prompt_pwd)
 end
 
 function fish_right_prompt --description 'Write out right of the prompt'
